ee9b54bf88f8c9dc2356664d60063248e9f30e42
Anna Clara <aannaedwm@gmail.com>
2025-08-26 09:26:27 -0300

    versão com calculadora

---
+++ nodejs/modulos/calculadora/app.js
@@ -0,0 +1,15 @@
+const funcoesMatematicas = require('./funcoes_matematicas');
+
+// Exemplo de uso:
+const resultadoSoma = funcoesMatematicas.somar(5, 3);
+console.log(`Resultado da soma: ${resultadoSoma}`);
+
+const resultadoSubtracao = funcoesMatematicas.subtrair(10, 4);
+console.log(`Resultado da subtração: ${resultadoSubtracao}`);
+
+const resultadoMultiplicacao = funcoesMatematicas.multiplicar(5, 3);
+console.log(`Resultado da multiplicação: ${resultadoMultiplicacao}`);
+
+const resultadoDivisao = funcoesMatematicas.dividir(10, 2);
+console.log(`Resultado da divisão: ${resultadoDivisao}`);
+
