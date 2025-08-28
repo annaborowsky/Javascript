8af5fca3b06c90c1a17d22e66de9ffd6bcaf39db
Anna Clara <aannaedwm@gmail.com>
2025-08-28 10:53:46 -0300

    versão com calculadora de idade

--- nodejs/modulos/calculadora/app.js
+++ nodejs/modulos/calculadora/app.js
@@ -1,5 +1,5 @@
 const funcoesMatematicas = require('./funcoes_matematicas');
-
+const calculadora_idade = require('./calculadora_idade');
 // Exemplo de uso:
 const resultadoSoma = funcoesMatematicas.somar(5, 3);
 console.log(`Resultado da soma: ${resultadoSoma}`);
@@ -13,3 +13,5 @@ console.log(`Resultado da multiplicação: ${resultadoMultiplicacao}`);
 const resultadoDivisao = funcoesMatematicas.dividir(10, 2);
 console.log(`Resultado da divisão: ${resultadoDivisao}`);
 
+const mensagemIdade = calculadora_idade.idade("Ana", 1990, 2025);
+console.log(mensagemIdade);
---
+++ nodejs/modulos/calculadora/calculadora_idade.js
@@ -0,0 +1,7 @@
+function idade(nome,anoNasc,anoAtual){
+    idade= anoAtual-anoNasc;
+    return `Olá, ${nome}. Em ${anoAtual} você terá ${idade} anos.`;
+};
+module.exports = {
+    idade
+};
