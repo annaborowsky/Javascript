2323e9ddbb10aa5cfc3b0db2ba0fa627d508067b
Anna Clara <aannaedwm@gmail.com>
2025-10-03 19:46:52 -0300

    versão com rotas

--- nodejs/modulos/calculadora/app.js
+++ nodejs/modulos/calculadora/app.js
@@ -1,21 +1,21 @@
 //const funcoes_matematicas = require('./funcoes_matematicas');
-import * as funcoes_matematicas from './funcoes_matematicas.js' ;
+import {soma, subtrai, multiplica, divide} from './modulos/funcoes_matematicas.js' ;
 
 //Exemplo de uso
-const resultadoSoma = funcoes_matematicas.soma(5, 3);
+const resultadoSoma =soma(5, 3);
 console.log(`Resultado da soma: ${resultadoSoma}`);
 
- const resultadoSubtracao = funcoes_matematicas.subtrai(10, 4);
+ const resultadoSubtracao = subtrai(10, 4);
  console.log(`Resultado da subtração: ${resultadoSubtracao}`);
 
- const resultadoMultiplicacao = funcoes_matematicas.multiplica(6, 7);
+ const resultadoMultiplicacao = multiplica(6, 7);
  console.log(`Resultado da multiplicação: ${resultadoMultiplicacao}`);
 
- const resultadoDivisao = funcoes_matematicas.divide(20, 4);
+ const resultadoDivisao = divide(20, 4);
  console.log(`Resultado da divisão: ${resultadoDivisao}`);
 
 //const calculadora_idade = require('./calculadora_idade');
-import * as calculadora_idade from './calculadora_idade.js' ;
+import * as calculadora_idade from './modulos/calculadora_idade.js' ;
 //Exemplo de uso
-const idadeLucas = calculadora_idade.idade('Lucas', 2024, 2000);
-console.log(idadeLucas);
+const idadeAnna = calculadora_idade.idade('Anna', 2000, 2025);
+console.log(idadeAnna);
---
+++ nodejs/modulos/calculadora/modulos/funcoes_matematicas.js
@@ -0,0 +1,20 @@
+export function soma(a, b) {
+    return a + b;
+}
+
+export function subtrai(a, b) {
+    return a - b;
+}
+
+export function multiplica(a, b) {
+    return a * b;
+}
+
+export function divide(a, b) {
+    if (b === 0) {
+        throw new Error('Divisão por zero não é permitida.');
+    }
+    return a / b;
+}
+
+// Outra forma de exportar todas as funções de uma vez
